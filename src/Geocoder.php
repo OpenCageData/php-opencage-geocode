546e3a632da29fadc79d5e41d42165b1a301a795
Edgars Strods <edgars@redbird.lv>
2017-12-23 23:22:08 +0200

    Added request optional parameter functionality

--- src/Geocoder.php
+++ src/Geocoder.php
@@ -6,9 +6,18 @@ use OpenCage\Geocoder\AbstractGeocoder;
 
 class Geocoder extends AbstractGeocoder
 {
-    public function geocode($query)
+    public function geocode($query, $optParams = [])
     {
         $url = self::URL . 'q=' . urlencode($query);
+        
+        if(is_array($optParams) && !empty($optParams))
+        {
+            foreach($optParams as $param => $paramValue)
+            {
+                $url .= '&'.$param.'=' . urlencode($paramValue);
+            }
+        }
+        
         if (empty($this->key)) {
             throw new \Exception('Missing API key');
         }
