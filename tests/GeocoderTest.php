7e1dac1cae7c9953b3ba72bce119a68bf3b56453
marc tobias <mtmail@gmx.net>
2026-03-31 01:54:32 +0200

    add tests for more error cases

--- tests/GeocoderTest.php
+++ tests/GeocoderTest.php
@@ -6,6 +6,13 @@ use OpenCage\Geocoder\Geocoder;
 
 class GeocoderTest extends \PHPUnit\Framework\TestCase
 {
+    // https://opencagedata.com/api#testingkeys
+    private const OPENCAGE_TEST_APIKEY_200 = '6d0e711d72d74daeb2b0bfd2a5cdfdba';
+    private const OPENCAGE_TEST_APIKEY_402 = '4372eff77b8343cebfc843eb4da4ddc4';
+    private const OPENCAGE_TEST_APIKEY_403_DISABLED = '2e10e5e828262eb243ec0b54681d699a';
+    private const OPENCAGE_TEST_APIKEY_403_IP_REJECTED = '6c79ee8e1ca44ad58ad1fc493ba9542f';
+    private const OPENCAGE_TEST_APIKEY_429 = 'd6d0f0065f4348a4bdfe4587ba02714b';
+
     public function testMissingKey(): void
     {
         $geocoder = new Geocoder();
@@ -29,8 +36,7 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
 
     public function testInvalidHost(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         try {
             $geocoder->setHost('www.example.com');
         } catch (\Exception $e) {
@@ -42,8 +48,7 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
 
     public function testNetworkRequestError(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         $geocoder->setHost('doesnotexist.opencagedata.com');
         $result = $geocoder->geocode('London');
         // print_r($result);
@@ -55,17 +60,38 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
 
     public function testOverQuota(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('4372eff77b8343cebfc843eb4da4ddc4');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_402);
         $result = $geocoder->geocode('Johannesburg');
         $this->assertEquals(402, $result['status']['code']);
         $this->assertEquals('quota exceeded', $result['status']['message']);
     }
 
+    public function testRateLimited(): void
+    {
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_429);
+        $result = $geocoder->geocode('Johannesburg');
+        $this->assertEquals(429, $result['status']['code']);
+    }
+
+    public function testIpRejected(): void
+    {
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_403_IP_REJECTED);
+        $result = $geocoder->geocode('Johannesburg');
+        $this->assertEquals(403, $result['status']['code']);
+        $this->assertStringContainsString('IP address rejected', $result['status']['message']);
+    }
+
+    public function testDisabledKey(): void
+    {
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_403_DISABLED);
+        $result = $geocoder->geocode('Johannesburg');
+        $this->assertEquals(403, $result['status']['code']);
+        $this->assertStringContainsString('disabled', $result['status']['message']);
+    }
+
     public function testLondon(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         $query = "82 Clerkenwell Road, London";
         $result = $geocoder->geocode($query);
 
@@ -77,8 +103,7 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
 
     public function testAsyncLondon(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         $promise = $geocoder->geocodeAsync("82 Clerkenwell Road, London");
         /** @var array{status: array{code: int, message: string}} $result */
         $result = $promise->wait();
@@ -89,8 +114,7 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
 
     public function testAsyncNetworkError(): void
     {
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         $geocoder->setHost('doesnotexist.opencagedata.com');
         $promise = $geocoder->geocodeAsync('London');
         /** @var array{status: array{code: int, message: string}} $result */
@@ -106,8 +130,7 @@ class GeocoderTest extends \PHPUnit\Framework\TestCase
         if (!$proxy) {
             $this->markTestSkipped('PROXY environment variable not set');
         }
-        // https://opencagedata.com/api#testingkeys
-        $geocoder = new Geocoder('6d0e711d72d74daeb2b0bfd2a5cdfdba');
+        $geocoder = new Geocoder(self::OPENCAGE_TEST_APIKEY_200);
         try {
             $geocoder->setProxy($proxy);
         } catch (\Exception $e) {
